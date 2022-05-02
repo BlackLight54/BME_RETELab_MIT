51511def5bb2d025e477a1ccb1ffefe2e83458bc
Farkas Martin <42645825+BlackLight54@users.noreply.github.com>
2022-05-02 15:45:32 +0200

    MIT4 1.1 Implemented periodic refrencespeed change

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,5 +1,8 @@
 package hu.bme.mit.train.controller;
 
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 import hu.bme.mit.train.interfaces.TrainController;
 
 public class TrainControllerImpl implements TrainController {
@@ -7,6 +10,18 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+
+	ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(2);
+	
+	public TrainControllerImpl(){
+		executor.scheduleAtFixedRate(new Runnable() {
+			@Override
+			public void run() {
+				followSpeed();;  
+			}
+		}, 0, 1, TimeUnit.SECONDS);
+	}
+
 
 	@Override
 	public void followSpeed() {
